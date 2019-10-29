07873d3de7c8c197f8f6d2c21f21d7ad0287ac2b
Nazar Sianko <nazar_senko@mail.ru>
2019-10-29 19:34:02 +0300

    Add files via upload

---
+++ task1.js
@@ -0,0 +1,19 @@
+<html>
+
+  <head>
+
+    <script>
+  let a = prompt('Введите  слово');
+  let c= a.toLowerCase();
+  let b = c;
+  let d = b.split('').reverse().join('').toLowerCase();
+    (c === d) ? alert('Палиндром') : alert('Не палиндром');
+    </script>
+
+   </head>
+
+  <body>
+
+  </body>
+
+</html>
